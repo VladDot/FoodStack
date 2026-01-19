ba30c714b0ee68027e2c630488a761715bb2576a
Vlad <vladkostirya4@gmail.com>
2026-01-19 18:25:21 +0200

    changed clsx for cn

--- src/shared/ui/auth/index.tsx
+++ src/shared/ui/auth/index.tsx
@@ -2,14 +2,11 @@
 
 import { ExitIcon } from "@/assets/icons";
 
+//TODO add avatar and user data
+
 export const HeaderProfile = ({}) => {
     return (
         <div className="flex items-center gap-10">
-            {/* <Avatar
-                src={avatar ? avatar :  DefaultAvatar}
-                alt={name}
-                className="w-8 h-8 rounded-full"
-            /> */}
             <div className="w-8 h-8 rounded-full bg-amber-700" />
             <button
                 onClick={() => console.log("logout")}
--- src/shared/ui/burger/styles.ts
+++ src/shared/ui/burger/styles.ts
@@ -1,23 +1,23 @@
-import clsx from "clsx";
+import { cn } from "@/shared/utils";
 
 interface IGetStyles {
     isOpen: boolean;
 }
 
 export const getStyles = ({ isOpen }: IGetStyles) => ({
-    burger: clsx("w-5 flex flex-col gap-1"),
+    burger: cn("w-5 flex flex-col gap-1"),
     span: "border-1 border-solid transparent transition-all duration-300 ease-in-out z-999",
 
-    top: clsx({ "rotate-45 translate-y-1": isOpen }),
-    middle: clsx({ "opacity-0": isOpen }),
-    bottom: clsx({ "-rotate-45 -translate-y-2": isOpen }),
+    top: cn({ "rotate-45 translate-y-1": isOpen }),
+    middle: cn({ "opacity-0": isOpen }),
+    bottom: cn({ "-rotate-45 -translate-y-2": isOpen }),
 
-    burgerMenu: clsx(
+    burgerMenu: cn(
         "fixed top-0 left-0 bg-linear-to-r from-lime-300 to-emerald-500 z-998 h-screen ",
         isOpen ? "animate-fade-in" : "animate-fade-out"
     ),
 
-    overlay: clsx("absolute top-0 left-0 w-screen h-screen  bg-black z-997  ", {
+    overlay: cn("absolute top-0 left-0 w-screen h-screen  bg-black z-997  ", {
         "animate-fade-in-opacity": isOpen,
         "animate-fade-out-opacity": !isOpen,
     }),
