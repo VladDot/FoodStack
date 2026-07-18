298417ea1b1f0cce4e964ba853edd096a497018e
Vlad <vladkostirya4@gmail.com>
2026-07-18 17:01:21 +0300

    Removing an unused type

--- src/shared/ui/button/types.ts
+++ src/shared/ui/button/types.ts
@@ -6,7 +6,6 @@ export type ButtonSize = "sm" | "md" | "icon";
 export type ButtonVariant = "primary" | "cta" | "outline" | "ghost" | "danger";
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     size?: ButtonSize;
-    asChild?: boolean;
     isActive?: boolean;
     isLoading?: boolean;
     leftIcon?: ReactNode;
