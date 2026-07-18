b8e537a31bbb135b568bb6db466ce02e443d4ab4
Vlad <vladkostirya4@gmail.com>
2026-07-18 17:04:15 +0300

    Removing excess wrapping

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -3,11 +3,5 @@ export default async function AuthLayout({
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <>
-            <main className=" flex-1 ">
-                <div className=" flex justify-center pt-10">{children}</div>
-            </main>
-        </>
-    );
+    return <div className=" flex justify-center pt-10">{children}</div>;
 }
